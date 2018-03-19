d297aaa26339439a238b102b19807ce0f4536cd9
Dray Lacy <dray@envylabs.com>
2018-03-19 12:12:04 -0400

    Update tests via ember-qunit-codemod

--- tests/unit/index-test.js
+++ tests/unit/index-test.js
@@ -2,72 +2,72 @@ import { module, test } from 'qunit';
 
 import * as polished from 'polished';
 
-module('Unit | Polished | Tests');
+module('Unit | Polished | Tests', function() {
+  [
+    // Mixins
+    'clearFix',
+    'ellipsis',
+    'fontFace',
+    'hiDPI',
+    'hideText',
+    'normalize',
+    'placeholder',
+    'radialGradient',
+    'retinaImage',
+    'selection',
+    'timingFunctions',
+    'triangle',
+    'wordWrap',
 
-[
-  // Mixins
-  'clearFix',
-  'ellipsis',
-  'fontFace',
-  'hiDPI',
-  'hideText',
-  'normalize',
-  'placeholder',
-  'radialGradient',
-  'retinaImage',
-  'selection',
-  'timingFunctions',
-  'triangle',
-  'wordWrap',
+    // Color
+    'adjustHue',
+    'complement',
+    'darken',
+    'desaturate',
+    'grayscale',
+    'hsl',
+    'hsla',
+    'invert',
+    'lighten',
+    'mix',
+    'opacify',
+    'parseToHsl',
+    'parseToRgb',
+    'rgb',
+    'rgba',
+    'saturate',
+    'setHue',
+    'setLightness',
+    'setSaturation',
+    'shade',
+    'tint',
+    'transparentize',
 
-  // Color
-  'adjustHue',
-  'complement',
-  'darken',
-  'desaturate',
-  'grayscale',
-  'hsl',
-  'hsla',
-  'invert',
-  'lighten',
-  'mix',
-  'opacify',
-  'parseToHsl',
-  'parseToRgb',
-  'rgb',
-  'rgba',
-  'saturate',
-  'setHue',
-  'setLightness',
-  'setSaturation',
-  'shade',
-  'tint',
-  'transparentize',
+    // Shorthands
+    'animation',
+    'backgroundImages',
+    'backgrounds',
+    'borderColor',
+    'borderRadius',
+    'borderStyle',
+    'borderWidth',
+    'buttons',
+    'margin',
+    'padding',
+    'position',
+    'size',
+    'textInputs',
+    'transitions',
 
-  // Shorthands
-  'animation',
-  'backgroundImages',
-  'backgrounds',
-  'borderColor',
-  'borderRadius',
-  'borderStyle',
-  'borderWidth',
-  'buttons',
-  'margin',
-  'padding',
-  'position',
-  'size',
-  'textInputs',
-  'transitions',
-
-  // Helpers
-  'directionalProperty',
-  'em',
-  'modularScale',
-  'rem',
-  'stripUnit'
-].forEach(method => {
-  test(`exports ${method} method`, function(assert) {
-    assert.equal(typeof polished[method], 'function');
+    // Helpers
+    'directionalProperty',
+    'em',
+    'modularScale',
+    'rem',
+    'stripUnit'
+  ].forEach(method => {
+    test(`exports ${method} method`, function(assert) {
+      assert.equal(typeof polished[method], 'function');
+    });
   });
 });
